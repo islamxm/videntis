5ec337e7ec7c62ae4b37368fa2d2dcd5b1949736
Islam <isxmad@gmail.com>
2023-02-22 13:33:54 +0300

    home page

--- #src/js/app.js
+++ #src/js/app.js
@@ -1,3 +1,9 @@
 import * as flsFuncs from './modules/functions.js';
+import heroSlider from './modules/heroSlider.js';
 
 flsFuncs.isWebp();
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    heroSlider();
+})
--- #src/js/modules/heroSlider.js
+++ #src/js/modules/heroSlider.js
@@ -5,7 +5,19 @@ const heroSlider = () => {
 
     if(el) {
         const slider = new Swiper(el, {
-
+            slidesPerView: 1,
+            modules: [Navigation, Pagination],
+            navigation: {
+                prevEl: '.hero__slider_nav-prev',
+                nextEl: '.hero__slider_nav-next',
+            },
+            speed: 700,
+            pagination: {
+                el: '.hero__slider_pag',
+                bulletClass: 'hero__slider_pag_item',
+                clickable: true,
+                bulletActiveClass: 'active'
+            }
         })
     }
 }
